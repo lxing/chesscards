57970ba528229334dd0568ac83e07ddca60d4065
Lawrence Xing <lawrence.xing@gmail.com>
2014-05-31 13:50:22 -0700

    Add card ui except draw

--- js/chesscards.js
+++ js/chesscards.js
@@ -102,28 +102,32 @@ function endTurn() {
   board.position(game.fen());
   events = deepCopy(DEFAULT_EVENTS);
 
-  if (cards['active'] !== null) {
+  if (cards['active'].length > 0) {
     cards['deck'].push(cards['active'])
-    cards['active'] = null;
+    cards['active'] = [];
+    renderCards();
   }
 
   renderStatus();
 }
 
 function drawCard(player, force) {
-  if ((force !== true && player != turn) || actionPoints[turn] < card.cost) return;
-  actionPoints[turn] -= DRAW_COST;
+  if (force !== true && (player != turn || actionPoints[turn] < DRAW_COST)
+    || cards['deck'].length === 0) return;
+  if (force !== true) actionPoints[turn] -= DRAW_COST;
 
   // True random, not FIFO
-  card = cards['deck'].splice(Math.floor(Math.random()*cards['deck'].length))
+  card = cards['deck'].splice(Math.floor(Math.random()*cards['deck'].length), 1)[0]
+  cards[player].push(card);
+
   renderStatus();
   renderCards();
 }
 
 function applyCard(card) {
-  if (cards['active'] !== null || actionPoints[turn] < card.cost) return false;
+  if (cards['active'].length > 0 || actionPoints[turn] < card.cost) return false;
   actionPoints[turn] -= card.cost;
-  cards['active'] = card;
+  cards['active'].push(card);
 
   // Override game logic here
   for (var eventType in DEFAULT_EVENTS) {
@@ -151,6 +155,31 @@ function renderStatus() {
 }
 
 function renderCards() {
+  for (var zone in cards) {
+    if (zone === 'deck') continue;
+    var container = $('#' + zone + 'cards');
+    container.html('');
+
+    for (var i=0; i<cards[zone].length; i++) {
+      var card = cards[zone][i];
+      var div = document.createElement('div');
+      $(div).addClass('card').addClass(zone + 'card')
+            .html(card['cost'] + 'AP | ' + card['name'] + ' | ' + card['description'])
+            .appendTo(container);
+
+      if (zone === 'active') {
+        $(div).addClass(turn + 'card');
+      } else {
+        $(div).click(makeOnCardClicked(i, zone));
+      }
+    }
+  }
+}
+
+function makeOnCardClicked(i, player) {
+  return function() {
+    applyCard(cards[player].splice(i, 1)[0]);
+  }
 }
 
 
@@ -161,12 +190,12 @@ var CHESSCARDS = [
   {
     name: 'Hasty Conscription',
     description: 'Place a pawn from your reserves anywhere on your first 3 ranks.',
-    cost: 2,
+    cost: 0,
     freq: 10,
     onDragStartSpare: function(source, piece, position) {
       if (sameColor(turn, piece) === false) return false;
       type = pieceStrToType(piece);
-      if (POINTS[type] > 1 || sparePieces[turn][type] < 1) return false;
+      if (PIECE_VALUES[type] > 1 || sparePieces[turn][type] < 1) return false;
     },
     onDropSpare: function(source, target, piece, newPos, oldPos) {
       type = pieceStrToType(piece);
@@ -179,13 +208,13 @@ var CHESSCARDS = [
   },
   {
     name: 'New Recruits',
-    description: 'Place a pawn or minor piece from your reserves anywhere on your first 3 ranks',
-    cost: 5,
+    description: 'Place a pawn or minor piece from your reserves anywhere on your first 3 ranks.',
+    cost: 0,
     freq: 10,
     onDragStartSpare: function(source, piece, position) {
       if (sameColor(turn, piece) === false) return false;
       type = pieceStrToType(piece);
-      if (POINTS[type] > 3 || sparePieces[turn][type] < 1) return false;
+      if (PIECE_VALUES[type] > 3 || sparePieces[turn][type] < 1) return false;
     },
     onDropSpare: function(source, target, piece, newPos, oldPos) {
       type = pieceStrToType(piece);
@@ -219,7 +248,7 @@ var sparePieces = {
   b: {p: 0, n: 0, b: 0, r: 0, q: 0}
 }
 var actionPoints = {w: 0, b: 0};
-cards = {w: [], b: [], active: null, deck: []};
+cards = {w: [], b: [], active: [], deck: []};
 var events = deepCopy(DEFAULT_EVENTS);
 
 function initStats() {
@@ -242,7 +271,7 @@ function initStats() {
 function initDeck() {
   for(var i=0; i<CHESSCARDS.length; i++) {
     card = CHESSCARDS[i];
-    for(var j=0; j<card.freq; j++) {
+    for(var j=0; j<card['freq']; j++) {
       cards['deck'].push(deepCopy(card));
     }
   }
